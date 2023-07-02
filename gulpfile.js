85250907351d3c9fa59aecbde932e39145ef0f17
Nyambi Blaise - Dəv / 237 <34681563+nyambiblaise@users.noreply.github.com>
2023-07-02 04:41:23 +0100

    Update gulpfile.js
    V

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
 gulp.task("licenses", async function () {
-  // this is to add Creative Tim licenses in the production mode for the minified js
+  // this isto add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
     .pipe(
