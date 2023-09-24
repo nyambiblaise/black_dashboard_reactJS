b51d5b036108b8f4c2e9204dcdb03e6d2012c2cd
Nyambi Blaise - Dəv / 237 <34681563+nyambiblaise@users.noreply.github.com>
2023-09-23 20:59:30 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
 import AdminLayout from "layouts/Admin/Admin.js";
 import RTLLayout from "layouts/RTL/RTL.js";
 
